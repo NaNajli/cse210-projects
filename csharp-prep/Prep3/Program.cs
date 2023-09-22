6c024bbf0f43a9ed8f4da996bb55212d74c45f1f
NaNajli <nadurodriguez2@gmail.com>
2023-09-22 03:58:45 -0300

    C# Prep 03, C# Prep 04

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
-}
+   
+      Random randomGenerator = new Random();
+      int guess = randomGenerator.Next(1,101);
+       
+      int magic = -1;
+      
+      while (guess != magic) 
+        { 
+          Console.Write(" What is the magic number?");
+          string magicNumber = Console.ReadLine();
+          magic = int.Parse(magicNumber);  
+         
+           if (guess < magic)
+           {
+             Console.WriteLine("Lower");
+           }
+           else if (guess > magic)
+           {
+             Console.WriteLine("Higher");
+           }
+           else 
+           {
+             Console.WriteLine("You guessed it!");       
+           } 
+          
+            
+        }
+    }}
+           
+        
+       
+      
+
+      
+
+      
+   
+       
+    
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,53 @@
 using System;
 
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+       //Console.WriteLine("Hello Prep4 World!");
+
+      List<int> Lnumbers = new List<int>(); 
+
+      Console.WriteLine("Enter a list of numbers, type 0 when finished."); 
+      
+      int numb = -1;
+      int i = 0;
+      while (numb != i)
+      {
+      Console.Write("Enter number: ");
+      string numbers = Console.ReadLine();
+      numb = int.Parse(numbers);
+       
+       if(numb !=i)
+        {
+          Lnumbers.Add(numb);   
+        }
+      }
+      
+      int sum = 0;
+      float average = 0;
+      foreach (int number in Lnumbers)
+       {
+        sum =Lnumbers.Sum();
+        average = ((float)sum)/Lnumbers.Count;
+       }
+       Console.WriteLine($"The sum is: {sum}"); 
+       Console.WriteLine($"The average is: {average}"); 
+      
+
+      foreach (int number in Lnumbers)
+      {
+          if (number > i)
+          {
+            i = number;
+          }
+      }
+      Console.WriteLine($"The max is: {i}");
     }
-}
+           
+     }
