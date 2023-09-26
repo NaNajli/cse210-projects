257d4a91b0f6ec7bc998f823e8d31819838247c7
NaNajli <nadurodriguez2@gmail.com>
2023-09-26 11:07:19 -0300

    Abstraction Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Tester";
+        job1._company = "BlueIt";
+        job1._startYear = 2009;
+        job1._endYear = 2021;
+
+
+        Job job2 = new Job();
+        job2._jobTitle = "software Ingeniera";
+        job2._company = "Team";
+        job2._startYear = 2021;
+        job2._endYear = 2023;
+
+
+
+        Resume resume = new Resume();
+        resume._name = "Nadia Rodriguez";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
+
+        
+
     }
 }
