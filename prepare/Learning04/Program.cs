52125a6bb5cc3594e0b5759719f9a63087db3b35
NaNajli <nadurodriguez2@gmail.com>
2023-10-26 23:51:33 -0300

    Inheritance Learning Activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+       // Console.WriteLine("Hello Learning04 World!");
+       Console.WriteLine();
+       Assignment assignment = new Assignment("Samuel Bennett","Multiplication");
+       Console.WriteLine(assignment.GetSummary());
+       
+       
+       MathAssignment assignment2 = new MathAssignment("Section", "7.3","Problems", "8-19");
+       Console.Write(assignment2.GetHomeworkList());
+       Console.WriteLine(assignment2.GetSummary());
+       
+       WritingAssignment assignment3 = new WritingAssignment("Mary Waters","European History","The Causes of World War II");
+       Console.Write(assignment3.GetStudentName());
+       Console.WriteLine(assignment3.GetTopic());
+       Console.WriteLine(assignment3.GetWritingInformation());
+       Console.WriteLine();
     }
 }
